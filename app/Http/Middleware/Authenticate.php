9deaee73585aaf35e15aa31ad6a289e1efa3eb48
alifakhdan <alifakhdan57@gmail.com>
2026-04-26 14:08:30 +0700

    menyesuaikan code dengan flutter mobile app

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,13 +5,19 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
-    {
-        return $request->expectsJson() ? null : route('login');
+    protected function redirectTo(Request $request): ?string{
+        if (! $request->expectsJson()) {
+            // Jika yang mengakses adalah API/Flutter, langsung tolak dengan 401
+            if ($request->is('api/*')) {
+                abort(401, 'Unauthenticated');
+            }
+            // Jika yang mengakses web browser biasa, arahkan ke login
+            return route('login');
+        }
+        return null;
     }
 }
